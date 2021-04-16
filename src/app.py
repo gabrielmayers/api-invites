5faaf4d565bac23772db68964e93fe716bdf628f
gabrielmayers <gabriel.mayers@hotmail.com>
2021-04-16 15:34:29 -0300

    l

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ invites_list = ['e9LGYg4yvT7jHzME', 'xvbZwaCjC4UrGKmG', 'xhyBaWmENMe6a5DZ', 'qJx
                 'tKFxKDHvUbue4rws', 'cFFJ79rLDJe3mr7k', 'HnL6KXayYJkXHeeV', '3gxm4ZvUJHBwPpBD', 'gF4vFxQjV2Z9fx2W',
                 'fhcMbk6B6LNWa2wY', '222LBNeE6gdSeYMM', '3XEz38HsybJu6V88', 'mAPpFnn94DpsLZLn', 'cB2mvzeE8aZTasWb']
 
-@app.route('/hi')
+@app.route('/', methods=['GET']) 
 def hi():
     return 'Ok!'
 
@@ -28,4 +28,4 @@ def generate_invite():
     return True
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
